a445fee1c4c9fa0fd4ac0cc250651255959b12cd
wanix <wanix.fr@gmail.com>
2015-03-09 14:02:45 +0100

    update

--- bin/package-tomcat7.sh
+++ bin/package-tomcat7.sh
@@ -27,7 +27,6 @@ typeset -x EASYFPM_OPTIONS=""
 typeset -x SRC_TOMCAT="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/apache-tomcat-_VERSION_TAG_.tar.gz"
 typeset -x SRC_TOMCAT_DEPLOYER="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/apache-tomcat-_VERSION_TAG_-deployer.tar.gz"
 typeset -x SRC_TOMCAT_FULLDOCS="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/apache-tomcat-_VERSION_TAG_-fulldocs.tar.gz"
-typeset -x SRC_TOMCAT_EMBED="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/embed/apache-tomcat-_VERSION_TAG_-embed.tar.gz"
 typeset -x SRC_TOMCAT_CAT_JMX_REM="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/extras/catalina-jmx-remote.jar"
 typeset -x SRC_TOMCAT_CAT_WS="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/extras/catalina-ws.jar"
 typeset -x SRC_TOMCAT_JULI_ADAPT="http://archive.apache.org/dist/tomcat/tomcat-7/v_VERSION_TAG_/bin/extras/tomcat-juli-adapters.jar"
@@ -58,7 +57,7 @@ f_clean_all(){
   #Clean all temporaries elements
   if [ -d ${TMP_DIR} ];
   then
-    rm -Rf ${TMP_DIR}
+    echo "rm -Rf ${TMP_DIR}"
   fi
   cd ${ACTUALDIR}
   unset SCRIPT_NAME BASE_DIR SCRIPT_TMP TMP_DIR SCRIPT_OUTPUT TOMCAT7_VERSION EASYFPM_CONF EASYFPM_OPTIONS ACTUALDIR
@@ -88,6 +87,10 @@ then
   [ -n "${OPT_EASYFPM_OPTIONS}" ] && EASYFPM_OPTIONS=${OPT_EASYFPM_OPTIONS}
   OPT_EASYFPM_CONF=$(grep "^EASYFPM_CONF=" ${CFG_FILE} | head -n 1 | sed 's/^EASYFPM_CONF=//')
   [ -n "${OPT_EASYFPM_CONF}" ] && EASYFPM_CONF=${OPT_EASYFPM_CONF}
+  OPT_SRC_TOMCAT=$(grep "^SRC_TOMCAT=" ${CFG_FILE} | head -n 1 | sed 's/^SRC_TOMCAT=//')
+  [ -n "${OPT_SRC_TOMCAT}" ] && SRC_TOMCAT=${OPT_SRC_TOMCAT}
+  OPT_SRC_TOMCAT_FULLDOCS=$(grep "^SRC_TOMCAT_FULLDOCS=" ${CFG_FILE} | head -n 1 | sed 's/^SRC_TOMCAT_FULLDOCS=//')
+  [ -n "${OPT_SRC_TOMCAT_FULLDOCS}" ] && SRC_TOMCAT_FULLDOCS=${OPT_SRC_TOMCAT_FULLDOCS}
 fi
 
 ## get and manage parameters
@@ -129,16 +132,21 @@ mkdir ${TMP_DIR}
 [ $? -ne 0 ] && printf "Error : can't create working tmp dir under ${SCRIPT_TMP}\n" && exit 1
 cd ${TMP_DIR}
 
-HTTP_SOURCES=$(echo ${SRC_TOMCAT} ${SRC_TOMCAT_DEPLOYER} ${SRC_TOMCAT_FULLDOCS} ${SRC_TOMCAT_EMBED} ${SRC_TOMCAT_CAT_JMX_REM} ${SRC_TOMCAT_CAT_WS} ${SRC_TOMCAT_JULI_ADAPT} ${SRC_TOMCAT_JULI} | sed "s/_VERSION_TAG_/${TOMCAT7_VERSION}/g")
+HTTP_SOURCES=$(echo ${SRC_TOMCAT} ${SRC_TOMCAT_FULLDOCS} | sed "s/_VERSION_TAG_/${TOMCAT7_VERSION}/g")
 for httpfile in ${HTTP_SOURCES};
 do
   [ -z "$(echo ${httpfile} | sed 's/ //g')" ] && continue
-  tarfile=$(echo ${httpfile} | sed -e 's/.+\/([ˆ\/])$/\1/')
+  tarfile=$(basename ${httpfile})
   echo "  Retrieving ${tarfile}"
-  wget ${httpfile} -O ${TMP_DIR}/${tarfile}
-  [ $? -ne 0 ] && printf "Error : can't retrieve ${httpfile}" && exit 1
-  wget ${httpfile}.md5 -O ${TMP_DIR}/${tarfile}.md5
-  [ $? -ne 0 ] && printf "Error : can't retrieve ${httpfile}.md5" && exit 1
-  md5sum -c ${tarfile}.md5
-  [ $? -ne 0 ] && printf "Error : integrity failure for ${tarfile}" && exit 1
+  wget -q ${httpfile} -O ${TMP_DIR}/${tarfile}
+  [ $? -ne 0 ] && printf "Error : can't retrieve ${httpfile}\n" && exit 1
+  wget -q ${httpfile}.md5 -O ${TMP_DIR}/${tarfile}.md5
+  [ $? -ne 0 ] && printf "Error : can't retrieve ${httpfile}.md5\n" && exit 1
+  md5sum --status -c ${tarfile}.md5
+  [ $? -ne 0 ] && printf "Error : integrity failure for ${tarfile}\n" && exit 1
+  tar xzf ${tarfile}
+  [ $? -ne 0 ] && printf "Error : can't unpack ${tarfile}\n" && exit 1
+  rm -f ${tarfile} ${tarfile}.md5
 done
+mv tomcat-7.0-doc/* apache-tomcat-${TOMCAT7_VERSION}/webapps/docs/
+[ $? -ne 0 ] && printf "Error : Can't prepare doc\n" && exit 1
